d4ebc7033ef02e5a418865bede99b87f1d57bbaa
sang5c <skah321@gmail.com>
2023-05-26 15:11:04 +0900

    init

---
+++ src/main/java/com/example/inflearnspringcoreprinciples/InflearnSpringCorePrinciplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.inflearnspringcoreprinciples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringCorePrinciplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringCorePrinciplesApplication.class, args);
+    }
+
+}
